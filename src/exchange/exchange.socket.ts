cc31b9486f7774182371dcf23e068587e987ae9d
seongeun <seongeun.cho.dev@gmail.com>
2024-03-31 22:38:46 +0900

    socket subscribe

--- src/exchange/exchange.socket.ts
+++ src/exchange/exchange.socket.ts
@@ -1,5 +1,11 @@
 import * as WebSocket from "ws";
 
+export interface WebSocketSubscription {
+  onData: (data: any) => void;
+  onError?: (error: Error) => void;
+  onClose?: () => void;
+}
+
 export class WebSocketClient {
   public ws;
 
@@ -10,6 +16,7 @@ export class WebSocketClient {
   queueSize: number = 1000;
   queue: any[] = [];
   converter: any;
+  private subscriptions: WebSocketSubscription[] = [];
 
   constructor(baseUrl: string, data: any, converter: any) {
     this.ws = new WebSocket(baseUrl);
@@ -19,38 +26,13 @@ export class WebSocketClient {
     this.converter = converter;
 
     this.ws.on("message", this.handleMessage.bind(this));
-  }
-
-  connectSocket() {
-    this.ws.on("open", () => {
-      this.ws.send(JSON.stringify(this.data));
-    });
-
-    this.ws.on("close", () => {
-      console.log("WebSocket connection closed");
-    });
-
-    this.ws.on("error", (error) => {
-      console.error("WebSocket error:", error);
-    });
+    this.ws.on("error", this.handleError.bind(this));
+    this.ws.on("close", this.handleClose.bind(this));
   }
 
   run() {
-    this.connectSocket();
-  }
-
-  async get() {
-    if (!this.alive) {
-      this.alive = true;
-      this.run();
-    }
-    return new Promise((resolve) => {
-      const intervalId = setInterval(() => {
-        if (this.queue.length > 0) {
-          clearInterval(intervalId);
-          resolve(this.queue.shift());
-        }
-      }, 100);
+    this.ws.on("open", () => {
+      this.ws.send(JSON.stringify(this.data));
     });
   }
 
@@ -61,12 +43,44 @@ export class WebSocketClient {
     }
   }
 
+  subscribe(subscription: WebSocketSubscription) {
+    this.subscriptions.push(subscription);
+
+    if (!this.alive) {
+      this.run();
+      this.alive = true;
+    }
+  }
+
+  unsubscribe(subscription: WebSocketSubscription) {
+    const index = this.subscriptions.indexOf(subscription);
+    if (index !== -1) {
+      this.subscriptions.splice(index, 1);
+    }
+  }
+
   private handleMessage(data: WebSocket.Data) {
     const receiveData = JSON.parse(data.toString("utf-8"));
     const convertedData = this.converter(receiveData);
-    this.queue.push(convertedData);
-    if (this.queue.length > this.queueSize) {
-      this.queue.shift();
-    }
+    // TODO:
+    this.subscriptions.forEach((subscription) => {
+      subscription.onData(convertedData);
+    });
+  }
+
+  private handleError(error: Error) {
+    this.subscriptions.forEach((subscription) => {
+      if (subscription.onError) {
+        subscription.onError(error);
+      }
+    });
+  }
+
+  private handleClose() {
+    this.subscriptions.forEach((subscription) => {
+      if (subscription.onClose) {
+        subscription.onClose();
+      }
+    });
   }
 }
--- src/upbit/upbit.spec.ts
+++ src/upbit/upbit.spec.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 import * as path from "path";
 import * as fs from "fs";
 import { UPBIT_PUBLIC_STREAM_DATA_TYPE, UPBIT_WEBSOCKET_URL } from "./upbit.constant";
-import { WebSocketClient } from "@exchange/exchange.socket";
+import { WebSocketClient, WebSocketSubscription } from "@exchange/exchange.socket";
 import { upbitSubscribeTickerConverter } from "./upbit.converter";
 
 describe("UPBIT", () => {
@@ -16,171 +16,63 @@ describe("UPBIT", () => {
     upbit = new Upbit(env.UPBIT_ACCESS_KEY, env.UPBIT_SECRET_KEY);
   });
 
-  // it("Fetch Tickers", async () => {
-  //   const result = await upbit.fetchTickers();
-  //   console.log(result);
-  // });
+  it("Fetch Tickers", async () => {
+    const result = await upbit.fetchTickers();
+    console.log(result);
+  });
 
-  // it("Fetch Wallet Status", async () => {
-  //   const result = await upbit.fetchWalletStatus();
-  //   console.log(result);
-  // });
+  it("Fetch Wallet Status", async () => {
+    const result = await upbit.fetchWalletStatus();
+    console.log(result);
+  });
 
-  // it("Fetch Balances", async () => {
-  //   const result = await upbit.fetchBalances();
-  //   console.log(result);
-  // });
+  it("Fetch Balances", async () => {
+    const result = await upbit.fetchBalances();
+    console.log(result);
+  });
 
-  // it("Fetch Deposit Histories", async () => {
-  //   const result = await upbit.fetchDepositHistory("ETH");
-  //   console.log(result);
-  // });
+  it("Fetch Deposit Histories", async () => {
+    const result = await upbit.fetchDepositHistory("ETH");
+    console.log(result);
+  });
 
-  // it("Fetch Withdraw History", async () => {
-  //   const result = await upbit.fetchWithdrawHistory("TRX");
-  //   console.log(result);
-  // });
+  it("Fetch Withdraw History", async () => {
+    const result = await upbit.fetchWithdrawHistory("TRX");
+    console.log(result);
+  });
 
-  // it("Fetch Deposit Addresses", async () => {
-  //   const result = await upbit.fetchDepositAddresses();
-  //   console.log(result);
-  // });
+  it("Fetch Deposit Addresses", async () => {
+    const result = await upbit.fetchDepositAddresses();
+    console.log(result);
+  });
 
-  // it("Fetch Order History", async () => {
-  //   const result = await upbit.fetchOrderHistory("TRX", "KRW");
-  //   console.log(result);
-  // });
-  // it("Fetch All Order History", async () => {
-  //   const result = await upbit.fetchAllOrderHistory();
-  //   // console.log(result);
-  //   const jsonResult = JSON.stringify(result, null, 2);
-  //   // console.log(jsonResult);
-  //   // Write the JSON string to data.json file
-  //   fs.writeFileSync("data.json", jsonResult);
-  // });
+  it("Fetch Order History", async () => {
+    const result = await upbit.fetchOrderHistory("TRX", "KRW");
+    console.log(result);
+  });
+  it("Fetch All Order History", async () => {
+    const result = await upbit.fetchAllOrderHistory();
+    // console.log(result);
+    const jsonResult = JSON.stringify(result, null, 2);
+    // console.log(jsonResult);
+    // Write the JSON string to data.json file
+    fs.writeFileSync("data.json", jsonResult);
+  });
   it("Stream public data", async (done) => {
     const ws = await upbit.subscribePublicData(UPBIT_PUBLIC_STREAM_DATA_TYPE.ticker);
-    while (true) {
-      const result = await ws.get();
-      console.log(result);
-    }
-  });
-  xit("asdf", async () => {
-    const data = [
-      { ticket: "4bbdd9a7-c6a9-4872-8e8a-bcd456a05bc8" },
-      {
-        type: "ticker",
-        codes: [
-          "KRW-BTC",
-          "KRW-ETH",
-          "BTC-ETH",
-          "BTC-XRP",
-          "BTC-ETC",
-          "BTC-CVC",
-          "BTC-DGB",
-          "BTC-SC",
-          "BTC-SNT",
-          "BTC-WAVES",
-          "BTC-NMR",
-          "BTC-XEM",
-          "BTC-QTUM",
-          "BTC-BAT",
-          "BTC-LSK",
-          "BTC-STEEM",
-          "BTC-DOGE",
-          "BTC-BNT",
-          "BTC-XLM",
-          "BTC-ARDR",
-          "BTC-ARK",
-          "BTC-STORJ",
-          "BTC-GRS",
-          "BTC-RLC",
-          "USDT-BTC",
-          "USDT-ETH",
-          "USDT-XRP",
-          "USDT-ETC",
-          "KRW-NEO",
-          "KRW-MTL",
-          "KRW-XRP",
-          "KRW-ETC",
-          "KRW-SNT",
-          "KRW-WAVES",
-          "KRW-XEM",
-          "KRW-QTUM",
-          "KRW-LSK",
-          "KRW-STEEM",
-          "KRW-XLM",
-          "KRW-ARDR",
-          "KRW-ARK",
-          "KRW-STORJ",
-          "KRW-GRS",
-          "KRW-ADA",
-          "BTC-ADA",
-          "BTC-MANA",
-          "KRW-SBD",
-          "BTC-SBD",
-          "KRW-POWR",
-          "BTC-POWR",
-          "KRW-BTG",
-          "USDT-ADA",
-          "BTC-DNT",
-          "BTC-ZRX",
-          "BTC-TRX",
-          "BTC-TUSD",
-          "BTC-LRC",
-          "KRW-ICX",
-          "KRW-EOS",
-          "USDT-TUSD",
-          "KRW-TRX",
-          "BTC-POLYX",
-          "USDT-SC",
-          "USDT-TRX",
-          "KRW-SC",
-          "KRW-ONT",
-          "KRW-ZIL",
-          "KRW-POLYX",
-          "KRW-ZRX",
-          "KRW-LOOM",
-          "BTC-BCH",
-          "USDT-BCH",
-          "KRW-BCH",
-          "BTC-HIFI",
-          "BTC-LOOM",
-          "KRW-BAT",
-          "KRW-IOST",
-          "USDT-DGB",
-          "KRW-CVC",
-          "KRW-IQ",
-          "KRW-IOTA",
-          "BTC-RVN",
-          "BTC-GO",
-          "BTC-UPP",
-          "BTC-ENJ",
-          "KRW-HIFI",
-          "KRW-ONG",
-          "KRW-GAS",
-          "BTC-MTL",
-          "KRW-UPP",
-          "KRW-ELF",
-          "USDT-DOGE",
-          "USDT-ZRX",
-          "USDT-RVN",
-          "USDT-BAT",
-          "KRW-KNC",
-          "BTC-MOC",
-          "BTC-ZIL",
-          "KRW-BSV",
-          "BTC-BSV",
-        ],
-        isOnlyRealtime: true,
+
+    const subscription: WebSocketSubscription = {
+      onData: (receivedData) => {
+        console.log("Received data:", receivedData);
       },
-    ];
-    const ws = new WebSocketClient(UPBIT_WEBSOCKET_URL, data, upbitSubscribeTickerConverter);
-    (async () => {
-      while (true) {
-        const result = await ws.get();
-        console.log(result);
-      }
-    })();
+      onError: (error) => {
+        console.log("error", error);
+      },
+      onClose: () => {
+        console.log("close");
+      },
+    };
+
+    ws.subscribe(subscription);
   });
 });
